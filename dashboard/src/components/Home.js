d2deaf3f0e521e15be6bd07aedb942dabd810571
tanish0007 <jangratanish0007@gmail.com>
2025-01-29 17:10:15 +0530

    add GenralContext.js

---
+++ dashboard/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Dashboard from "./Dashboard";
+import Topbar from "./Topbar";
+
+const Home = () => {
+  return (
+    <>
+      <Topbar />
+      <Dashboard />
+    </>
+  );
+};
+
+export default Home;
