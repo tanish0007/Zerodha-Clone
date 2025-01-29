74fae9732a622d0892729c944f970fb9af4f0cbf
tanish0007 <jangratanish0007@gmail.com>
2025-01-29 23:42:09 +0530

    make changes to holdings from data

--- dashboard/src/components/Orders.js
+++ dashboard/src/components/Orders.js
@@ -3,16 +3,13 @@ import { Link } from "react-router-dom";
 
 function Orders(){
   return (
-    // <div className="orders">
-    //   <div className="no-orders">
-    //     <p>You haven't placed any orders today</p>
+    <div className="orders">
+      <div className="no-orders">
+        <p>You haven't placed any orders today</p>
 
-    //     <Link to={"/"} className="btn">
-    //       Get started
-    //     </Link>
-    //   </div>
-    // </div>
-    <h1>Orders</h1>
+        <Link to={"/"} className="btn">Get started</Link>
+      </div>
+    </div>
   );
 };
 
