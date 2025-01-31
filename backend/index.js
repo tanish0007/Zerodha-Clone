d899c23439b7be4873e21470642c93acc841ba39
tanish0007 <jangratanish0007@gmail.com>
2025-01-31 18:03:08 +0530

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
+require('dotenv').config();
+
 const express = require("express");
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3003;
+const uri = process.env.MONGO_URL;
 
 const app = express();
 
-app.listen(3003, ()=>{
-    console.log("App started")
+app.listen(PORT, ()=>{
+    console.log("App started");
+    mongoose.connect(uri);
+    console.log("Database Connected!!");    
 })
