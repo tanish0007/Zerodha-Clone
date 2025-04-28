458a2daa80dad41134750603fba93b8bcbb1c1bd
tanish0007 <jangratanish0007@gmail.com>
2025-04-29 00:20:10 +0530

    dashboard is also good now

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Home from "./components/Home";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+
+const App = () => {
+  // useEffect(() => {
+  //     const token = localStorage.getItem("token");
+  //     if(!token){
+  //         window.location.href = "http://localhost:3000/login";
+  //     }
+  // }, []);
+
+  return (
     <BrowserRouter>
       <Routes>
         <Route path="/*" element={<Home />} />
       </Routes>
     </BrowserRouter>
-);
+  )
+}
+
+root.render( <App />);
